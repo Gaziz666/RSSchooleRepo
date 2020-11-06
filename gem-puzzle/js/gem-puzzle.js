aef6faaa3aa091124cd22b2fd322706bdfcd9ef6
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-11-06 22:55:29 +0300

    feat: update gam-puzzle

--- gem-puzzle/js/gem-puzzle.js
+++ gem-puzzle/js/gem-puzzle.js
@@ -36,6 +36,48 @@ export default class GemPuzzle {
     this.chipArr.sort(() => Math.random() - 0.5);
     this.chipArr.forEach((chip) => {
       this.container.append(chip.chip);
+      // eventListener mousedown on chip
+      chip.chip.onmousedown = this.preHandleEvent;
+
     });
+  }
+
+  preHandleEvent = (e) => {
+    let chip = e.target.closest('.chip');
+    let chipClone = chip.cloneNode(true)
+    let shiftX = e.clientX - chip.getBoundingClientRect().left
+    let shiftY = e.clientY - chip.getBoundingClientRect().top
+
+    document.body.append(chipClone)
+    chip.classList.add('empty')
+
+    chipClone.classList.add('dragging')
+
+    chipClone.ondragstart = () => false
+
+    moveAt(e.pageX, e.pageY)
+
+    function moveAt(pageX, pageY) {
+      chipClone.style.left = pageX - shiftX + 'px'
+      chipClone.style.top = pageY - shiftY + 'px'
+    }
+
+    function onMouseMove(e) {
+      moveAt(e.pageX, e.pageY);
+
+      chipClone.hidden = true;
+      let elemBelow = document.elementFromPoint(e.clientX, e.clientY)
+      chipClone.hidden = false
+      if (!elemBelow) return;
+
+      let elemEmpty = elemBelow.closest('.empty')
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    chipClone.onmouseup = () => {
+      document.removeEventListener('mousemove', onMouseMove)
+      chipClone.onmouseup = null
+    }
   }
 }
