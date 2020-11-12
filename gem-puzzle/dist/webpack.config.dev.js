33cc76438ce89444ccf36ff92949479efef85a4c
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-11-12 19:51:53 +0300

    feat: add config

--- gem-puzzle/dist/webpack.config.dev.js
+++ gem-puzzle/dist/webpack.config.dev.js
@@ -1,11 +1,9 @@
-"use strict";
-
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: './js/script.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
